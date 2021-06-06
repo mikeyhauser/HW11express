390598c9bab8648b67800f93e735b53103e3ff82
Mikey Hauser <mikeyhauser@gmail.com>
2021-06-06 13:18:52 -0400

    const PORT = process.env.PORT || 3001;

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./routes/apiRoutes.js');
 const htmlRoutes = require('./routes/htmlRoutes.js');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
